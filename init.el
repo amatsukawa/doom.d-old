26168f01110169cf3397daf154262ff6b3d338dc
Akihiro Matsukawa <akihiro.matsukawa@gmail.com>
2020-08-24 15:15:56 -0400

    helm fuzzy and treemacs

--- init.el
+++ init.el
@@ -21,7 +21,8 @@
 
        :completion
        company           ; the ultimate code completion backend
-       helm              ; the *other* search engine for love and life
+       (helm              ; the *other* search engine for love and life
+        +fuzzy)
        ;;ido               ; the other *other* search engine...
        ;;ivy               ; a search engine for love and life
 
@@ -42,7 +43,7 @@
        ophints           ; highlight the region an operation acts on
        (popup +defaults)   ; tame sudden yet inevitable temporary windows
        ;;tabs              ; a tab bar for Emacs
-       ;;treemacs          ; a project drawer, like neotree but cooler
+       treemacs          ; a project drawer, like neotree but cooler
        ;;unicode           ; extended unicode support for various languages
        vc-gutter         ; vcs diff in the fringe
        vi-tilde-fringe   ; fringe tildes to mark beyond EOB
